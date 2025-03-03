56d27d2eb6d8a1954dd7f725a01f1317abcb7bde
Neddddy <dima.duka@ukr.net>
2025-03-03 22:14:21 +0200

    hw2.py

--- hw2.py
+++ hw2.py
@@ -7,10 +7,12 @@ def get_numbers_ticket():
         minn = int(input("Введіть мінімальне число для лотереї: "))
         maxx = int(input("Введіть максимальне число для лотереї: "))
         quantityy = int(input("Введіть кількість чисел для лотереї: "))
-        if minn > 0 and maxx < 1000 and quantityy < maxx:
+        if minn > 0 and maxx < 1000 and minn < quantityy < maxx:
             while len(lottery_numbers) < quantityy:
                 lottery_numbers.add(random.randint(minn, maxx))
             return f"Ваші лотерейні числа: {sorted(list(lottery_numbers))}"
+        else:
+            return "Некоректний запис даних!!!"
     except ValueError:
         return "Некоректний запис даних!!!"
 print(get_numbers_ticket())
