15bcee330471b0acdbf2c313624bfc10752b5b3c
Neddddy <dima.duka@ukr.net>
2025-03-04 22:41:24 +0200

    hw2.py

--- hw2.py
+++ hw2.py
@@ -1,18 +1,15 @@
 # Завдання 2
 import random
 
-def get_numbers_ticket():
+def get_numbers_ticket(min, max, quantity):
     lottery_numbers = set()
     try:
-        minn = int(input("Введіть мінімальне число для лотереї: "))
-        maxx = int(input("Введіть максимальне число для лотереї: "))
-        quantityy = int(input("Введіть кількість чисел для лотереї: "))
-        if minn > 0 and maxx < 1000 and minn < quantityy < maxx:
-            while len(lottery_numbers) < quantityy:
-                lottery_numbers.add(random.randint(minn, maxx))
+        if min >= 1 and max <= 1000 and min < quantity < max:
+            while len(lottery_numbers) < quantity:
+                lottery_numbers.add(random.randint(min, max))
             return f"Ваші лотерейні числа: {sorted(list(lottery_numbers))}"
         else:
             return "Некоректний запис даних!!!"
     except ValueError:
         return "Некоректний запис даних!!!"
-print(get_numbers_ticket())
+print(get_numbers_ticket(2, 10, 20))
